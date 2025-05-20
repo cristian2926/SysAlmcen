95bd5a226fd652fd5b80dc76345382f044a7d861
cristian2926 <christcabana@gmail.com>
2025-05-20 18:38:14 -0500

    Primer commit con todas las carpetas

---
+++ src/main/java/pe/edu/upeu/sysalmacen/servicio/IMarcaService.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.sysalmacen.servicio;
+
+import pe.edu.upeu.sysalmacen.modelo.Marca;
+
+public interface IMarcaService extends ICrudGenericoService<Marca, Long>{
+
+    Long periodoIdMax();
+}
